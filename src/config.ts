e7ebbff2e3ca3a2005add36ae9e0f3f6d7ba2003
nmani28 <nigilya.mani@gmail.com>
2026-02-14 09:02:26 -0500

    update the home page

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,13 @@ export const SITE = {
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Nigilya Mani",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
+  postPerIndex: 1,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
+  showArchives: false,
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: true,
